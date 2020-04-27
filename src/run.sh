8149ad478cdafcd7e8efc327671922454149685f
John Thompson <john.gt82@gmail.com>
2020-04-27 10:22:38 +1000

    fix: Gemfile and install.sh: renamed to run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Battleships install script
+# Battleships install run script
 
-echo "Welcome to **Battleships Terminal App Installer**"
+echo "Welcome to **Battleships Terminal App**"
 
-echo "Lets begin the install now"
+echo "Lets begin the install now so the game can run"
 
 #Checking if git is installed 
 git --version 2>&1 >/dev/null 
@@ -25,17 +25,17 @@ cd ./Battleships_TerminalApp/src
 
 echo "Time to ensure all required gems are installed"
 
-
+#check if bundle is installed
 bundle --version 2>&1 >/dev/null 
 BUNDLE_IS_AVAILABLE=$?
 
 if [ $BUNDLE_IS_AVAILABLE -eq 0 ]; then
-bundle install
+bundle install --gemfile="Gemfile"
 echo "All gems have been installed, running application for the first time now"
 ruby battleships.rb
 else
-echo "Please install \"bundle gem\" before trying to install the game"
-echo "It can be install by running the:" 
+echo "Please install \"bundle gem\" before trying to run the game"
+echo "bundler can be install by running the:" 
 echo "\"gem install bundler\" commmand"
 exit 1
 fi
